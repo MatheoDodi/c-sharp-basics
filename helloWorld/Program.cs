b0a1f20ee02e5cf8633b2191c1e721e113d48caa
MatthewDodi <matthew.dodi@gmail.com>
2019-03-07 20:48:43 -0800

    type: first commit, initial files

---
+++ helloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace helloWorld
+{
+    class MainClass
+    {
+        private const string sentance = "Hello World";
+
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine(sentance);
+        }
+    }
+}
