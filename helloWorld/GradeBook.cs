70c290ca8392e1a71b302d01287e69acde344672
MatthewDodi <matthew.dodi@gmail.com>
2019-03-11 22:21:35 -0700

    Type: feat - created new GradeBook.cs class that will handle grades of students

---
+++ helloWorld/GradeBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace helloWorld
+{
+    public class GradeBook
+    {
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        List<float> grades;
+    }
+}
